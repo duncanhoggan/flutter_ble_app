73d9d95d7a51a0300461f5d9407d37d77201ec00
Duncan <duncan.hoggan@nologostudios.com>
2020-01-29 14:27:51 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,230 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final String SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
+  final String CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+
+  final String TARGET_DEVICE_NAME = "esp32_matrix";
+
+//  final String TARGET_DEVICE_NAME = "ESPBLE";
+
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  StreamSubscription<ScanResult> scanSubScription;
+
+  BluetoothDevice targetDevice;
+  BluetoothCharacteristic targetCharacteristic;
+
+  String connectionText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    startScan();
+  }
+
+  startScan() {
+    setState(() {
+      connectionText = "Start Scanning";
+    });
+
+    scanSubScription = flutterBlue.scan().listen((scanResult) {
+      if (scanResult.device.name == TARGET_DEVICE_NAME) {
+        print('DEVICE found');
+        stopScan();
+        setState(() {
+          connectionText = "Found Target Device";
+        });
+
+        targetDevice = scanResult.device;
+        connectToDevice();
+      }
+    }, onDone: () => stopScan());
+  }
+
+  stopScan() {
+    scanSubScription?.cancel();
+    scanSubScription = null;
+  }
+
+  connectToDevice() async {
+    if (targetDevice == null) return;
+
+    setState(() {
+      connectionText = "Device Connecting";
+    });
+
+    await targetDevice.connect();
+    print('DEVICE CONNECTED');
+    setState(() {
+      connectionText = "Device Connected";
+    });
+
+    discoverServices();
+  }
+
+  disconnectFromDevice() {
+    if (targetDevice == null) return;
+
+    targetDevice.disconnect();
+
+    setState(() {
+      connectionText = "Device Disconnected";
+    });
+  }
+
+  discoverServices() async {
+    if (targetDevice == null) return;
+
+    List<BluetoothService> services = await targetDevice.discoverServices();
+    services.forEach((service) {
+      // do something with service
+      if (service.uuid.toString() == SERVICE_UUID) {
+        service.characteristics.forEach((characteristic) {
+          if (characteristic.uuid.toString() == CHARACTERISTIC_UUID) {
+            targetCharacteristic = characteristic;
+
+            setState(() {
+              connectionText = "All Ready with ${targetDevice.name}";
+            });
+          }
+        });
+      }
+    });
+  }
+
+  writeData(String data) {
+    if (targetCharacteristic == null) return;
+
+    List<int> bytes = utf8.encode(data);
+
+    targetCharacteristic.write(bytes);
+  }
+
+  TextEditingController _textFieldController = TextEditingController();
+
+  Future<String> _displayDialog(BuildContext context) async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Set Text'),
+            content: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(hintText: ""),
+            ),
+            actions: <Widget>[
+              new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              new FlatButton(
+                child: new Text('SEND'),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(_textFieldController.text);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(8),
+            child: Text(
+              connectionText,
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          Expanded(
+            child: GridView.count(
+              // Create a grid with 2 columns. If you change the scrollDirection to
+              // horizontal, this produces 2 rows.
+              crossAxisCount: 3,
+              // Generate 100 widgets that display their index in the List.
+              children: List.generate(20, (index) {
+                return Center(
+                  child: Material(
+                    color: Colors.orange,
+                    child: new InkWell(
+                      onTap: () {
+                        writeData("0|${index.toString()}");
+                        print("tapped");
+                      },
+                      child: new Container(
+                        width: 120.0,
+                        height: 120.0,
+                        child: Center(
+                          child: Text(
+                            '$index',
+                            style: Theme.of(context).textTheme.headline,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              }),
+            ),
+          )
+        ],
+      ),
+
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            {_displayDialog(context).then((txt) => {writeData("1|$txt")})},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
